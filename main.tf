54e7528743ae7f0534b723383a20b3faacf3380c
Agbera <126923646+Agbera@users.noreply.github.com>
2023-03-31 19:29:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 #readme
 provider "google"{}
-resource "google_compute_global_address” “default" {
+resource "google_compute_global_address" "default" {
   project="olagt-380915"
   name="global-server-ip"
  
